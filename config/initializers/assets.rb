78019cde10737ebecbab699632b56c1a6c627756
Mark Megahey <mark@indee.io>
2016-11-18 11:52:16 +0000

    Attempting Editor

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,12 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+Rails.application.config.assets.precompile += %w( DataTables/datatables.css )
+Rails.application.config.assets.precompile += %w( Editor-1.5.6/css/editor.dataTables.css )
+Rails.application.config.assets.precompile += %w( DataTables/datatables.js )
+Rails.application.config.assets.precompile += %w( Editor-1.5.6/js/dataTables.editor.js )
+
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
